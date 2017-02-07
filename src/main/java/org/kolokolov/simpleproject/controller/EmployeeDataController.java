b99479bb4ce0688383eeba5764e331865eeb5115
kolokolov <kolokolov_a@ukr.net>
2017-02-07 17:14:21 +0200

    Add log4j2

--- src/main/java/org/kolokolov/simpleproject/controller/EmployeeDataController.java
+++ src/main/java/org/kolokolov/simpleproject/controller/EmployeeDataController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kolokolov.simpleproject.model.Employee;
 import org.kolokolov.simpleproject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +15,13 @@ import org.springframework.stereotype.Controller;
 @ManagedBean
 public class EmployeeDataController {
     
+    private static Logger logger = LogManager.getLogger();
+    
     @Autowired
     private EmployeeService employeeService;
 
     public List<Employee> getEmployees() {
-        System.out.println("---getEmployees---");
+        logger.debug("getEmployees method runs");
         return employeeService.getAllEmployees();
     }
 }
