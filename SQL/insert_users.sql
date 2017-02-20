05983f40dc2a4d2489a4d27a9e44ad3371690e8b
AlexKolokolov <kolokolov_a@ukr.net>
2017-02-20 17:40:33 +0200

    Add encrypted password storing in DB

--- SQL/insert_users.sql
+++ SQL/insert_users.sql
@@ -1,3 +1,3 @@
-INSERT INTO user_details VALUES (1, 'admin', 'admin', 1, 0);
-INSERT INTO user_details VALUES (2, 'user', 'user', 0, 0);
+INSERT INTO user_details VALUES (1, 'admin', '$2a$10$gbOvEyfxaegaI4cYxvD3HuidXqNrn10gwTnUibbTt8K0ESHDFihxi', 1, 0);
+INSERT INTO user_details VALUES (2, 'user', '$2a$10$I6ZB7FwQAxHV4LhnaL8wcuO5p4g7un0lPjYr0jxpphCXcd7q51yI2', 0, 0);
 COMMIT;
